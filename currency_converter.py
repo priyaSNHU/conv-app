398cb7c978e18d609a829bfd0d7abb989b57426d
Julien Chaffraix <julien.chaffraix@gmail.com>
2018-05-07 21:03:18 -0700

    Make the code run
    The code was wrongly assuming that the form
    parameters were passed in request.form but
    they were actually passed as URL parameters
    in request.args.
    
    Besides this, it's a lot of small
    mistakes and typos.

--- currency_converter.py
+++ currency_converter.py
@@ -11,18 +11,16 @@ def run_server():
  
 
 @app.route("/result", methods=['GET','POST'])
-
 def convert():
-    currencies_from = request.form.get('currency_from')
-    currencies_to = request.form.get('currency_to')
-    amt = str(request.form.get('amount'))
+    currencies_from = request.args.get('currency_from')
+    currencies_to = request.args.get('currency_to')
+    amount = float(request.args.get('amount'))
     currency_rates = {'INR': 66.7681, 'EUR': 1.1969, 'AUD': 0.7538 , 'USD' : 1}
  
-    converted_rate = (float(amt) * float(currency_rate[currencies_from])  * float(currency_rate[Currencies_to]))
+    converted_rate = (float(amount) * float(currency_rates[currencies_from])  * float(currency_rates[currencies_to]))
 
-    data = {'amount':amt, 'currency_from': currencies_from, 'currency_to': currencies_to, 'converted_amt': converted_rate}
+    data = {'amount':amount, 'currency_from': currencies_from, 'currency_to': currencies_to, 'converted_amt': converted_rate}
             
- 
     return render_template('result.html', data=data )
  
 if __name__ == "__main__":
